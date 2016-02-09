1f61f483959d9ee09ef1d9d893a6293b96bb410c
Heath Nail <sitedyno@gmail.com>
2016-02-09 18:26:43 -0500

    Add typehint to Plugin::resolve()

--- src/Plugin.php
+++ src/Plugin.php
@@ -162,11 +162,11 @@ class Plugin extends AbstractPlugin
      * Handles a successful request for video data.
      *
      * @param string $url URL of the request
-     * @param string $data Response body
+     * @param \GuzzleHttp\Message\Response $data Response body
      * @param \Phergie\Irc\EventInterface $event
      * @param \Phergie\Irc\Bot\React\EventQueueInterface $queue
      */
-    public function resolve($url, $data, Event $event, Queue $queue)
+    public function resolve($url, \GuzzleHttp\Message\Response $data, Event $event, Queue $queue)
     {
         $logger = $this->getLogger();
         $json = json_decode($data->getBody());
